3adf821367ed4fb22e66e4649fc419884170a0b0
Abubakr Shoaliev <shoalievabubakr@gmail.com>
2022-09-01 21:14:12 +0500

    forest monster!

---
+++ src/main/java/myProject/stick1/ForestMonster.java
@@ -0,0 +1,13 @@
+package myProject.stick1;
+
+public class ForestMonster {
+    String name;
+    int age;
+    int health;
+
+    public ForestMonster(String name, int age, int health) {
+        this.name = name;
+        this.age = age;
+        this.health = health;
+    }
+}
