bd527dff836a0d720654175cd60e068997f34910
Abubakr Shoaliev <shoalievabubakr@gmail.com>
2022-09-01 21:36:48 +0500

    added an empty constructor to LightSide class

--- src/main/java/myProject/realScene/LightSide.java
+++ src/main/java/myProject/realScene/LightSide.java
@@ -1,6 +1,8 @@
 package myProject.realScene;
 
 public class LightSide extends Entity{
+    public LightSide() {}
+
   public LightSide(String name , int age, int health) {
     super (name, age, health);
   }
