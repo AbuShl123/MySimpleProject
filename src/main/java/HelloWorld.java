f4005f0724600552c9209df06314218863c122e2
Abubakr Shoaliev <shoalievabubakr@gmail.com>
2022-09-30 16:38:37 +0500

    some OCA book examples - Chapter 2 - Statements

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,38 +1,7 @@
 import java.util.*;
 
 public class HelloWorld {
-    // static field
-    static int legs = 2;
-
-    // instance fields
-    public int a;
-    public int b;
-    public String name;
-
-
-    // instance method
-    public void run() {
-        System.out.println("running");
-    }
-
-
-    public static void main(String[] args) {
-        HelloWorld obj = new HelloWorld();
-        obj.name = "Abu";
-
-
-        HelloWorld obj2 = new HelloWorld();
-        obj2.name = "Amin";
-
-
-        System.out.println(HelloWorld.legs);
-        System.out.println(obj.legs);
-        System.out.println(obj2.legs);
-
-        obj2.legs = 3;
-        System.out.println(obj2.legs);
-        System.out.println(obj.legs);
-        System.out.println(HelloWorld.legs);
+    public static void main(String[] args) throws InterruptedException{
 
     }
 }
